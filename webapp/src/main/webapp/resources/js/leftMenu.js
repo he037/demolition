25688fed48b6b1df23795edf1bdb9a0db92189b4
hewei <037hewei@gmail.com>
2016-03-23 17:39:36 +0800

    ui

---
+++ webapp/src/main/webapp/resources/js/leftMenu.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+	
+	function unfoldMenu(pn, cn){
+		var p = $('span.flip'), c = $('div.panel'), cc = $('div.panel-current');
+		
+			p.removeClass('flip-current');
+			cc.hide(200, function(){
+				$(this).removeAttr('style').removeClass('panel-current');
+			})
+			if(c.index(cn) != c.index(cc)){
+			pn.addClass('flip-current');
+			cn.show(200, function(){
+				$(this).removeAttr('style').addClass('panel-current');
+			});
+		}
+	}
+	
+	function menuHandle(){
+		$('span.flip').click(function(){
+			var pn = $(this), cn = pn.next();
+			unfoldMenu(pn, cn);
+		});
+	}
+	
+	//设置默认下当前展开
+	function menuCurrent(){
+		var idx = $('input.menu-code-index').val(), m, pn, cn, p = $('span.flip'), c = $('div.panel'), cc = $('div.panel-current');
+		if(/c(\d)+/.test(idx)){ //判断c（十进制）条件
+			m = $('a[data-service-index="' + idx + '"]').addClass('current');
+			cn = m.parents('div.panel');
+			pn = cn.prev();
+			unfoldMenu(pn, cn);
+		}
+	}
+	
+	menuCurrent();
+	menuHandle();
+	
+});
+
+
