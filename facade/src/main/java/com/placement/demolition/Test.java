84899e19a7ac54c4968d26a8faa321dd7ec4c749
hewei <037hewei@gmail.com>
2016-03-24 08:10:41 +0800

    update

--- facade/src/main/java/com/placement/demolition/Test.java
+++
@@ -1,5 +0,0 @@
-package com.placement.demolition;
-
-public class Test {
-
-}
---
+++ facade/src/main/java/com/placement/demolition/commons/component/JsonSerializer.java
@@ -0,0 +1,55 @@
+package com.placement.demolition.commons.component;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import redis.clients.util.SafeEncoder;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.placement.demolition.commons.component.RedisComponent.Serializer;
+
+
+public class JsonSerializer implements Serializer {
+
+	private Logger logger = LoggerFactory.getLogger(JsonSerializer.class);
+
+	@Override
+	public byte[] serialzation(Object object) {
+		if (object == null)
+			return null;
+		try {
+			return SafeEncoder.encode(JSONObject.toJSONString(object));
+		} catch (Exception e) {
+			logger.error("", e);
+			return null;
+		}
+	}
+
+	@Override
+	public <T> T deserialization(byte[] byteArray, Class<T> c) {
+		if (byteArray == null || byteArray.length == 0)
+			return null;
+		try {
+			return JSON.parseObject(SafeEncoder.encode(byteArray), c);
+		} catch (Exception e) {
+			logger.error("", e);
+			return null;
+		}
+	}
+
+	@Override
+	public <E> List<E> deserializationList(byte[] byteArray, Class<E> elementC) {
+		if (byteArray == null || byteArray.length == 0)
+			return null;
+		try {
+			return JSON.parseArray(SafeEncoder.encode(byteArray), elementC);
+		} catch (Exception e) {
+			logger.error("", e);
+			return null;
+		}
+	}
+
+}
