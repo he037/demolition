f2bcae3e774c7b560acd83801d23689b725f1cae
he037 <037hewei@gmail.com>
2016-05-03 20:44:28 +0800

    测试基类

---
+++ test/src/main/java/com/placement/demolition/service/BaseServiceTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C), 2002-2015, 铜板街
+ * FileName: BaseServiceTest.java
+ * Author:   Administrator
+ * Date:     2015年8月4日 上午9:54:15
+ * Description: //模块目的、功能描述      
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名             修改时间            版本号                  描述
+ */
+package com.placement.demolition.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈功能详细描述〉
+ *
+ * @author Administrator
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+@ContextConfiguration(locations = "classpath:META-INF/spring/demolition-context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+public class BaseServiceTest {
+    
+
+
+}
